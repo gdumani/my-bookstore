88c4fae3d57a70ff2b1aa398d4f63a43d5531a9a
gdumani <gdumani@gmail.com>
2022-03-30 16:47:21 -0600

    add files for books reducer and configure store

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Categories = () => <button type="button">Check status</button>;
+const Categories = () => <h1>Under Construction</h1>;
 
 export default Categories;
