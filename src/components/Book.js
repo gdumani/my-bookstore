85b7015f09241ba804882514a33e0e7d1ebcd7aa
gdumani <gdumani@gmail.com>
2022-03-30 17:38:48 -0600

    implement remove book action

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
+  const dispatch = useDispatch;
   const {
     id, title, author, category,
   } = props;
@@ -10,7 +13,13 @@ const Book = (props) => {
       <h5>{category}</h5>
       <h4>{title}</h4>
       <h6>{author}</h6>
-      <button id={id} type="button">Remove </button>
+      <button
+        id={id}
+        onClick={() => dispatch(removeBook(id))}
+        type="button"
+      >
+        Remove
+      </button>
     </li>
   );
 };
