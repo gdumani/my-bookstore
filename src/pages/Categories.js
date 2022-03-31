d617582e270ff21fcdb2828baa841fcb2409de70
gdumani <gdumani@gmail.com>
2022-03-31 10:55:47 -0600

    add action to categies button

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => <h1>Under Construction</h1>;
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categoriesReducer);
+  console.log(categories);
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => dispatch(checkStatus())}
+      >
+        Check status
+      </button>
+      <h3>{categories}</h3>
 
+    </div>
+
+  );
+};
 export default Categories;
