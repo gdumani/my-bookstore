7aa4622d2266492ed5f87ab6b9e59fe6c2167436
gdumani <gdumani@gmail.com>
2022-03-30 23:12:18 -0600

    create categories reducer and combine it into the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
+import categoriesReducer from './categories/categories'
 
 const reducer = combineReducers({
   booksReducer,
+  categoriesReducer,
 });
 
 const store = createStore(
