225577f2fd27314d26f91a812f06c4c483d0d184
gdumani <gdumani@gmail.com>
2022-04-01 10:12:31 -0600

    style index and app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './style/App.css';
 import NavBar from './components/NavBar';
 import Categories from './pages/Categories';
 import Books from './pages/books';
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </Router>
+    <div className="container">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
