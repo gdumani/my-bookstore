267820a6105b45e6429008fbb643df5e8303e393
gdumani <gdumani@gmail.com>
2022-04-01 10:20:01 -0600

    style Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../style/navbar.css';
+import head from '../pages/head.svg';
 
 const NavBar = () => (
   <div className="nav">
@@ -13,6 +14,7 @@ const NavBar = () => (
         <Link to="/categories">CATEGORIES</Link>
       </li>
     </ul>
+    <img className="user-image" alt="Profile" src={head} />
   </div>
 );
 export default NavBar;
